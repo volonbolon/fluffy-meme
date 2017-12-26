b4fc3c1b30e8dd4f97f79d9be18d2a04d2bdf933
Ariel Rodriguez <ariel@volonbolon.me>
2017-12-26 17:00:55 -0300

    Initial Commit

---
+++ LinkIt/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LinkIt
+//
+//  Created by Ariel Rodriguez on 26/12/2017.
+//  Copyright © 2017 Ariel Rodriguez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LinkIt/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LinkIt
+//
+//  Created by Ariel Rodriguez on 26/12/2017.
+//  Copyright © 2017 Ariel Rodriguez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
